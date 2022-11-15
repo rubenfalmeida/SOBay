4afd5c1a51099da30676aa63d9ca9eda88c16512
rubenfalmeida <118128028+rubenfalmeida@users.noreply.github.com>
2022-11-15 12:03:10 +0000

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,14 +1,15 @@
-struct cliente
+typedef struct 
 {
     char nome[100];
     char password[100];
     int saldo;
-};
+}cliente;
 
-struct item
+typedef struct 
 {
     char nome[100];
     char categoria[100];
     int preco;
     int precocompraja;
-};
+    int tempovenda;
+}item;
